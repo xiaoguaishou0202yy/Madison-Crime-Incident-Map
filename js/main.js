55c6074f13b3b17c04c2c8021a021b1ebe3272a3
xiaoguaishou0202yy@gmail.com <xiaoguaishou0202yy@gmail.com>
2024-07-19 20:34:57 -0500

    Neighborhood Boundries

--- js/main.js
+++ js/main.js
@@ -2,63 +2,67 @@ window.onload = function() {
     setMap(); 
 };
 
+
+
 function setMap() {
-    //map frame dimensions
-    var width = window.innerWidth * 0.5,
-    height = 680;
+    // Initialize Leaflet map
+    var map = L.map('map').setView([43.1, -89.4], 12); // Adjust the center and zoom level as needed
 
-    //create new svg container for the map
-    var map = d3.select(".map-container")
-        .append("svg")
-        .attr("class", "map")
-        .attr("width", width)
-        .attr("height", height);
+    // Add OpenStreetMap tile layer
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
 
-    //create Albers equal area conic projection
-    var projection = d3.geoAlbers()
-        .center([12.64, 43])
-        .rotate([102, 0, 0])
-        .parallels([40, 75])
-        .scale(100000)
-        .translate([width / 2, height / 2]);
-    
-    var path = d3.geoPath()
-        .projection(projection);
-
-    //use Promise.all to parallelize asynchronous data loading
-    var promises = [];    
-    promises.push(d3.csv("data/Species.csv")); //load attributes from csv    
-    promises.push(d3.json("data/Neighborhood_Associations.topojson")); //load choropleth spatial data    
-    promises.push(d3.csv("data/species_az.csv")); //load attributes from csv 
-
-    Promise.all(promises).then(callback);
-
-    function callback(data) {    
-        var csvData = data[0];   
-        var states = data[1]; 
-        var csvSpecies = data[2];
-
-        console.log("CSV Data:", csvData);
+    // Load and display the neighborhood associations data
+    d3.json("data/Neighborhood_Associations.topojson").then(function(states) {
         console.log("States TopoJSON:", states);
-        console.log("CSV Species Data:", csvSpecies);
 
         // Translate TopoJSON to GeoJSON
         var neighborhoodAssociations = topojson.feature(states, states.objects.Neighborhood_Associations).features;
 
         console.log("Neighborhood Associations GeoJSON:", neighborhoodAssociations);
 
-        //add Neighborhood Associations to map
-        var neighborhoods = map.selectAll(".neighborhood")
-            .data(neighborhoodAssociations)
-            .enter()
-            .append("path")
-            .attr("class", "neighborhood")
-            .attr("d", path)
-            .style("fill", "none")
-            .style("stroke", "black");
+        // Create an SVG layer for D3 overlay
+        var svg = d3.select(map.getPanes().overlayPane).append("svg");
+        var g = svg.append("g").attr("class", "leaflet-zoom-hide");
 
-        console.log("Neighborhood paths added:", neighborhoods);
-    }
+        // Create a project function to convert latitude and longitude to map positions
+        function projectPoint(x, y) {
+            var point = map.latLngToLayerPoint(new L.LatLng(y, x));
+            this.stream.point(point.x, point.y);
+        }
+
+        var transform = d3.geoTransform({point: projectPoint});
+        var path = d3.geoPath().projection(transform);
+
+        var feature = g.selectAll("path")
+            .data(neighborhoodAssociations)
+            .enter().append("path");
+
+        map.on("viewreset", reset);
+        map.on("zoom", reset); // Added event listener for zoom (highlighted)
+        reset();
+
+        // Function to reset and reposition the SVG layer
+        function reset() {
+            var bounds = path.bounds({type: "FeatureCollection", features: neighborhoodAssociations}),
+                topLeft = bounds[0],
+                bottomRight = bounds[1];
+
+            svg.attr("width", bottomRight[0] - topLeft[0])
+                .attr("height", bottomRight[1] - topLeft[1])
+                .style("left", topLeft[0] + "px")
+                .style("top", topLeft[1] + "px");
+
+            g.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
+
+            feature.attr("d", path)
+                .style("fill", "none")
+                .style("stroke", "black");
+        }
+
+        console.log("Neighborhood paths added to map.");
+    })
 }
 
 function continueToMap() {

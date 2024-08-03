35ada5fe7162fdbb5755301957a3d58016236065
xiaoguaishou0202yy@gmail.com <xiaoguaishou0202yy@gmail.com>
2024-08-03 02:44:30 -0500

    Update Style

--- js/main.js
+++ js/main.js
@@ -6,11 +6,13 @@ window.onload = function() {
 
 function setMap() {
     // Initialize Leaflet map
-    map = L.map('map').setView([43.1, -89.4], 12);
+    map = L.map('map').setView([43.0831, -89.4012], 11.5);
 
     // Add OpenStreetMap tile layer
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: 'abcd',
+        maxZoom: 19
     }).addTo(map);
 
     // Use Promise.all to parallelize asynchronous data loading
